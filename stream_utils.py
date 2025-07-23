4d570ad47fc757de50e668f505931dabc5e4446c
Aditya Kumar Sinha <adityauv11@gmail.com>
2025-07-23 11:43:18 +0530

    backend utils

--- stream_utils.py
+++ stream_utils.py
@@ -2,10 +2,24 @@ import cv2
 
 
 class Streaming():
-    def __init__(self):
+    def __init__(self, in_source=None, out_source=None, fps=None, blur_strength=None, background="none"):
+        self.input_source = in_source
+        self.output_source = out_source
+        self.fps = fps
+        self.blur_strength = blur_strength
+        self.background = background
+
+    def update_streaming_config(self, in_source=None, out_source=None, fps=None, blur_strength=None, background="none"):
+        self.input_source = in_source
+        self.output_source = out_source
+        self.fps = fps
+        self.blur_strength = blur_strength
+        self.background = background
+
+    def stream_video(self):
         pass
 
-    def list_availavle_devices(self):
+    def list_available_devices(self):
         devices = []
         for i in range(5):
             cap = cv2.VideoCapture(i)
